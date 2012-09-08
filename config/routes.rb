ad5504b431776e19d40bb67edf1c073b9923d015
Sergey Lukin <contact@sergeylukin.com>
2012-09-08 16:43:41 -0400

    Created /about and /contact pages according to their Tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Phase1::Application.routes.draw do
   root :to => 'static_pages#home'
 
   match 'help' => "static_pages#help"
+  match 'about' => "static_pages#about"
+  match 'contact' => "static_pages#contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
