7d8e262d79685b6cb7493419741f9e8681e81fe0
Sergey Lukin <contact@sergeylukin.com>
2012-09-08 16:42:43 -0400

    Add basic Tests for /about and /contact pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,4 +15,16 @@ describe "Static pages" do
     it { should have_selector 'title', text: full_title('Help') }
     it { should have_selector 'h1', text: "Help" }
   end
+
+  describe "About Us page" do
+    before { visit about_path }
+    it { should have_selector 'title', text: full_title('About Us') }
+    it { should have_selector 'h1', text: "About Us" }
+  end
+
+  describe "Contact Us page" do
+    before { visit contact_path }
+    it { should have_selector 'title', text: full_title('Contact') }
+    it { should have_selector 'h1', text: "Contact Us" }
+  end
 end
