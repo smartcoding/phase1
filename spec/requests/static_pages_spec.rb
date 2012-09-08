4f912f3e3f3e2a82fcc44bdde359ef68c30a3b1e
Sergey Lukin <contact@sergeylukin.com>
2012-09-08 13:24:41 -0400

    Add basic Tests for / and /help pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+    it { should have_selector 'title', text: full_title('') }
+    it { should have_selector 'h1', text: "Welcome" }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+    it { should have_selector 'title', text: full_title('Help') }
+    it { should have_selector 'h1', text: "Help" }
+  end
+end
